25be1d58c81f6f6f5dc40900862ae97afc5204cf
emi <emazzurque@gmail.com>
2020-04-26 17:28:33 -0300

    portfolio

--- web_personal/web_personal/urls.py
+++ web_personal/web_personal/urls.py
@@ -1,7 +1,7 @@
 """web_personal URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+#jerem@cfp.com
 from django.contrib import admin
 from django.urls import path
+from core import views as core_views
+from portfolio import views as portfolio_views
+from django.conf import settings
 
-urlpatterns = [
+urlpatterns = [    
+    path('', core_views.home, name='home'),
+    path('about-me/',core_views.about ,name='about'),
+    path('porfolio/',portfolio_views.portfolio ,name='portfolio'),
+    path('contact/',core_views.contact ,name='contact'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
